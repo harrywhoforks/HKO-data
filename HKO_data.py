ba87a79b5ab529fb345f838003b4dddb7b860acc
harrywhoforks <phwongharris@gmail.com>
2018-11-01 17:01:52 +0800

    Trying to make two defs and use year to recognize when to use the other def

--- HKO_data.py
+++ HKO_data.py
@@ -8,17 +8,23 @@ Created on Thu Nov  1 10:05:31 2018
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 
 start_url = 'https://www.hko.gov.hk/tide/ttext.htm'
 prefix = 'https://www.hko.gov.hk/tide/'
+high_low = 'high&low'
+hourly = 'hourly'
 
-def get_table(url, filename):
+def get_table_high_low(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     table = soup.find('pre').text
-    with open(filename, 'w') as f:
-        f.write(table)
+    return table
+
+def get_table_hourly(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    table = soup.find('pre').text
+    return table
 
 if __name__ == "__main__":
     soup = BeautifulSoup(requests.get(start_url).text, 'lxml')
@@ -28,5 +34,11 @@ if __name__ == "__main__":
         row_name = columns[0].text.replace('*', '').replace(' ', '_')
         for column in columns[1:]:
             links = column.find_all('a')
+            year = None
             for link in links:
-                get_table(prefix+link['href'], "{}_{}.tsv".format(row_name,link.text))
+                if link.text != year:
+                    year = link.text
+                    data = get_table_high_low(prefix+link['href'])
+                elif link.text == year:
+                    year = link.text
+                    data = get_table_hourly(prefix+link['href'])
