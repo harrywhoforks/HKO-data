54524cba7c856429f2f2a8c5f19371ce94dec219
harrywhoforks <phwongharris@gmail.com>
2018-11-02 15:25:22 +0800

    I need help converting the DataFrame table

--- HKO_data.py
+++ HKO_data.py
@@ -7,38 +7,40 @@ Created on Thu Nov  1 10:05:31 2018
 
 import requests
 from bs4 import BeautifulSoup
+import re
 import pandas as pd
 
 start_url = 'https://www.hko.gov.hk/tide/ttext.htm'
 prefix = 'https://www.hko.gov.hk/tide/'
-high_low = 'high&low'
-hourly = 'hourly'
 
-def get_table_high_low(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    table = soup.find('pre').text
-    return table
-
-def get_table_hourly(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    table = soup.find('pre').text
-    return table
-
-if __name__ == "__main__":
-    soup = BeautifulSoup(requests.get(start_url).text, 'lxml')
+def table_parser(soup):
     rows = soup.find_all('tr')[1:]
     for row in rows:
-        columns = row.find_all('td')
-        row_name = columns[0].text.replace('*', '').replace(' ', '_')
-        for column in columns[1:]:
-            links = column.find_all('a')
-            year = None
-            for link in links:
-                if link.text != year:
-                    year = link.text
-                    data = get_table_high_low(prefix+link['href'])
-                elif link.text == year:
-                    year = link.text
-                    data = get_table_hourly(prefix+link['href'])
+        cells = row.find_all('td')
+        row_place = cells[0].text.replace('*', '').replace(' ', '_')
+        links = cells[1].find_all('a')
+        for link in links:
+            get_table(prefix+link['href'],'highlow',row_place) #"{}_{}_{}.tsv".format('highlow',row_place,link.text))
+        links = cells[2].find_all('a')
+        for link in links:
+            get_table(prefix+link['href'], 'hourly', row_place)    
+               
+def get_table(url, tide_type, row_place):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    table = [re.split(r"\s{3,}", line) for line in soup.find('pre').text.splitlines() if not line == "" if not '_' in line if not any(char.isalpha() for char in line)]
+    if tide_type == 'highlow':
+        pass
+        #highlow_covert(pd.DataFrame(table))
+    if tide_type == 'hourly':
+        hourly_covert(pd.DataFrame(table))
+
+# Convert the table format to three columns: date, time and tide
+def hourly_covert(table):
+    day_hour = range(1,24)
+
+#def highlow_convert(table):
+    
+if __name__ == "__main__":
+    soup = BeautifulSoup(requests.get(start_url).text, 'lxml')
+    table_parser(soup)
